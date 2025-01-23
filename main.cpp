4a490031114d23f1b5d2dd56107557009291f8b5
hazarath23 <125800907+hazarath23@users.noreply.github.com>
2025-01-23 21:25:29 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ int main()
     int z = 165;
     void* ptr = static_cast<void*>(&y);
     void* ptr1 = static_cast<void*>(&z);
-    int g=SymTable_put(head,typ.c_str(),ptr);
+    SymTable_init(&head,typ.c_str(),ptr);
     int g2=SymTable_put(head,typ2.c_str(),ptr1);
-    int* intPtr = static_cast<int*>(SymTable_replace(head,const_cast<char*>("char"),ptr1));
+    int* intPtr = static_cast<int*>(SymTable_replace(head,const_cast<char*>("int"),ptr1));
     int result = *intPtr; 
     cout<<result<<endl;
-    int* intPtr1 = static_cast<int*>(SymTable_remove(head,"int"));
+    int* intPtr1 = static_cast<int*>(SymTable_remove(&head,"int"));
     int result1 = *intPtr1; 
     cout<<result1<<endl;
     SymTable_free(head);
